20ece329faa6950a0eb2a017d0b956532254f5ca
Ben A <ben.atkinson@lexicondigital.com.au>
2022-12-15 18:43:23 +1100

    fix day 10 part 2 test after updates

--- src/2022/day/12/day_12.ts
+++ src/2022/day/12/day_12.ts
@@ -4,8 +4,8 @@ import { part2dfs } from "./part_2_dfs";
 import { part2bfs } from "./part_2_bfs";
 
 export function day_12() {
-  // console.log(`║ Day 12 - Part 1 - DFS: ${part1dfs()}`);
-  // console.log(`║ Day 12 - Part 2 - DFS: ${part2dfs()}`);
+  console.log(`║ Day 12 - Part 1 - DFS: ${part1dfs()}`);
+  console.log(`║ Day 12 - Part 2 - DFS: ${part2dfs()}`);
 
   console.log(`║ Day 12 - Part 1 - BFS: ${part1bfs()}`);
   console.log(`║ Day 12 - Part 1 - BFS: ${part2bfs()}`);
