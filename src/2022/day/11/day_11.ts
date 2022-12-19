17895ca7fa53bd462eae46bf3f4cc5ebbb8671b4
Ben A <ben.atkinson@lexicondigital.com.au>
2022-12-19 12:31:32 +1100

    UTILS: 🏎️🐘

--- src/2022/day/11/day_11.ts
+++ src/2022/day/11/day_11.ts
@@ -1,7 +1,8 @@
+import { timeExec } from "../../../utils/perf";
 import { part1 } from "./part_1";
 import { part2 } from "./part_2";
 
 export function day_11() {
-  console.log(`║ Day 11 - Part 1: ${part1()}`);
-  console.log(`║ Day 11 - Part 2: ${part2()}`);
+  console.log(`║ Day 11 - Part 1: ${timeExec(part1)}`);
+  console.log(`║ Day 11 - Part 2: ${timeExec(part2)}`);
 }
--- src/2022/day/12/day_12.ts
+++ src/2022/day/12/day_12.ts
@@ -2,11 +2,12 @@ import { part1dfs } from "./part_1_dfs";
 import { part1bfs } from "./part_1_bfs";
 import { part2dfs } from "./part_2_dfs";
 import { part2bfs } from "./part_2_bfs";
+import { timeExec } from "../../../utils/perf";
 
 export function day_12() {
-  console.log(`║ Day 12 - Part 1 - DFS: ${part1dfs()}`);
-  console.log(`║ Day 12 - Part 2 - DFS: ${part2dfs()}`);
+  console.log(`║ Day 12 - Part 1 - DFS: ${timeExec(part1dfs)}`);
+  console.log(`║ Day 12 - Part 2 - DFS: ${timeExec(part2dfs)}`);
 
-  console.log(`║ Day 12 - Part 1 - BFS: ${part1bfs()}`);
-  console.log(`║ Day 12 - Part 1 - BFS: ${part2bfs()}`);
+  console.log(`║ Day 12 - Part 1 - BFS: ${timeExec(part1bfs)}`);
+  console.log(`║ Day 12 - Part 2 - BFS: ${timeExec(part2bfs)}`);
 }
