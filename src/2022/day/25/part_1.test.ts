20c6c091f736ca86e239deb03dc0c7e7fea1f153
Ben A <ben.atkinson@lexicondigital.com.au>
2022-12-26 13:32:48 +1100

    DAY 25: 🔥🎈🧝‍♀️⭐️🍊🦌🎄

--- src/2022/day/25/part_1.test.ts
+++ src/2022/day/25/part_1.test.ts
@@ -2,8 +2,24 @@ import { part1 } from "./part_1";
 
 describe("day 25 - part 1", () => {
   it("calculates the right answer!", () => {
-    const input = ["1", "2", "3"];
+    const input = [
+      // 3125   625   125   25    5  1
+      //  1     -2     -1    0   -1  2
+      "1=-0-2",
+      "12111",
+      "2=0=",
+      "21",
+      "2=01",
+      "111",
+      "20012",
+      "112",
+      "1=-1=",
+      "1-12",
+      "12",
+      "1=",
+      "122",
+    ];
     const result = part1(input);
-    expect(result).toBe("1");
+    expect(result).toBe("2=-1=0");
   });
 });
